227ca62adc9b20e209e2bd5fa7ac65923bbb4e3a
raphael <raphael.silva31@fatec.sp.gov.br>
2025-07-18 18:57:09 -0300

    Commit das alterações

--- Api/Infraestrutura/Db/DbContexto.cs
+++ Api/Infraestrutura/Db/DbContexto.cs
@@ -17,26 +17,24 @@ public class DbContexto : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Administrador>().HasData(
-            new Administrador {
+            new Administrador
+            {
                 Id = 1,
                 Email = "administrador@teste.com",
                 Senha = "123456",
                 Perfil = "Adm"
-             }
+            }
         );
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        if(!optionsBuilder.IsConfigured)
+        if (!optionsBuilder.IsConfigured)
         {
-            var stringConexao = _configuracaoAppSettings.GetConnectionString("MySql")?.ToString();
-            if(!string.IsNullOrEmpty(stringConexao))
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("SqlServer")?.ToString();
+            if (!string.IsNullOrEmpty(stringConexao))
             {
-                optionsBuilder.UseMySql(
-                    stringConexao,
-                    ServerVersion.AutoDetect(stringConexao)
-                );
+                optionsBuilder.UseSqlServer(stringConexao);
             }
         }
     }
